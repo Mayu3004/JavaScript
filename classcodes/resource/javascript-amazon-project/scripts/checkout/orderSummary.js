4a389a1f0219a1f419776ef66f7805f90c359d0c
mayur.limbhore-coditas <mayur.limbhore@coditas.com>
2024-02-23 12:57:54 +0530

    Make payment summary interactive

--- classcodes/resource/javascript-amazon-project/scripts/checkout/orderSummary.js
+++ classcodes/resource/javascript-amazon-project/scripts/checkout/orderSummary.js
@@ -4,6 +4,7 @@ import { formatCurrency } from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions,getDeliveryOption } from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 // hello();
 // const today = dayjs();
@@ -118,8 +119,10 @@ export function renderOrderSummary(){
                 const productId = link.dataset.productId;
                 removeFromCart(productId);
 
+
             const container =  document.querySelector(`.js-cart-item-container-${productId}`)
                 container.remove();
+                renderPaymentSummary();
             });
 
         });
@@ -131,6 +134,7 @@ export function renderOrderSummary(){
             element.addEventListener('click',()=>{
                 updateDeliveryOption(productId, deliveryOptionId);
                 renderOrderSummary();
+                renderPaymentSummary();
             });
         });
 }
